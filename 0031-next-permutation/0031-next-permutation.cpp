c12b450800716ff775a2df52f402a379114925c5
ojhaddeepak <116076273+ojhaddeepak@users.noreply.github.com>
2025-03-13 10:07:33 +0530

    Time: 0 ms (100%), Space: 15.7 MB (33.29%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,32 +2,18 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        //step 1//finding pivot index
-        int idx = -1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                idx =i;
-                break;
+        int i = n - 2;
+        while (~i && nums[i] >= nums[i + 1]) {
+            --i;
+        }
+        if (~i) {
+            for (int j = n - 1; j > i; --j) {
+                if (nums[j] > nums[i]) {
+                    swap(nums[i], nums[j]);
+                    break;
+                }
             }
         }
-    if(idx==-1){//if are already greatest
-        reverse(nums.begin(),nums.end());
-        return;
-    } 
-    //2  sorting reverse after pivot
-      reverse(nums.begin()+idx+1,nums.end());
-      //3 swapping idx and idx+1
-      //3finding the just greater element than idx
-      int j = -1;
-      for(int i=idx+1;i<n;i++){
-        if(nums[i]>nums[idx]){
-            j=i;
-            break;
-        }
-      }
-      //3 swapping idx and j
-      int temp = nums[idx];
-      nums[idx] = nums[j];
-      nums[j] = temp;
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
